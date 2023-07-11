42f776b70b4ca341d1e05176287fce7e82d6b05d
Shahzaib1432 <95637172+Shahzaib1432@users.noreply.github.com>
2023-07-11 14:28:40 +0500

    Update example.py

--- example.py
+++ example.py
@@ -1,14 +1,7 @@
-from PyView import *
+from trufle import *
 window = Window()
-window.size(600, 400)
 
-myButton = _combobox_(window)
-myButton.place(10,10)
-
-label = Label(window, text='hello')
-label.place(100, 100)
-
-button1 = Button(window, text="do something", hover_color='red', command=lambda: label.configure(text='test'))
-button1.place(200,200)
+cb = CheckBox(window)
+cb.place(100,100)
 
 window.run()
